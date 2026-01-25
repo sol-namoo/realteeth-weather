e5491a4c891d84981c612355e052e3341e038204
sol-namoo <sollee922@gmail.com>
2026-01-25 00:04:58 +0000

    fix: remove invalid place exports

--- src/entities/place/index.ts
+++ src/entities/place/index.ts
@@ -1,5 +1,1 @@
-export type { FavoritePlace } from './model/types';
-
-export { makePlaceId, parsePlaceId } from './model/placeIds';
-
 export { addFavorite, canAddMore, removeFavorite, updateFavorite } from './model/favorites';
